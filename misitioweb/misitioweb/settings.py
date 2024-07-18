66cd6c302e1e5750d737f7fc32b8f08d694c7191
Daniel Feito <danfeito@gmail.com>
2024-07-18 19:14:43 +0200

    setting mail

--- misitioweb/misitioweb/settings.py
+++ misitioweb/misitioweb/settings.py
@@ -140,8 +140,9 @@ MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'primozkretinsky@gmail.com'
+EMAIL_HOST_PASSWORD = 'ytwl bptu buem tbbx' #past the key or password app here
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'primozkretinsky@gmail.com'
-EMAIL_HOST_PASSWORD = 'H3nryCh1n@sk1'
+DEFAULT_FROM_EMAIL = 'default from email'
 
